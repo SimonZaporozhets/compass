a3d586e2470a23dcdd8a1a1f489a0f7ad5b369f7
Simon Zaporozhets <56390222+SimonZaporozhets@users.noreply.github.com>
2019-10-27 15:43:46 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -72,18 +72,18 @@ $(document).ready(function(){
 
 });
 
-    function sendAjaxForm(result_form, ajax_form, url) {
-    $.ajax({
-        url:     url, //url страницы (action_ajax_form.php)
-        type:     "POST", //метод отправки
-        dataType: "html", //формат данных
-        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
-        success: function(response) { //Данные отправлены успешно
-        	result = $.parseJSON(response);
-        	$('#result_form').html('Имя: '+result.name+'<br>Email: '+result.email+'<br>Телефон: '+result.phone);
-    	},
-    	error: function(response) { // Данные не отправлены
-            $('#result_form').html('Ошибка. Данные не отправлены.');
-    	}
- 	});
-}
+//     function sendAjaxForm(result_form, ajax_form, url) {
+//     $.ajax({
+//         url:     url, //url страницы (action_ajax_form.php)
+//         type:     "POST", //метод отправки
+//         dataType: "html", //формат данных
+//         data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
+//         success: function(response) { //Данные отправлены успешно
+//         	result = $.parseJSON(response);
+//         	$('#result_form').html('Имя: '+result.name+'<br>Email: '+result.email+'<br>Телефон: '+result.phone);
+//     	},
+//     	error: function(response) { // Данные не отправлены
+//             $('#result_form').html('Ошибка. Данные не отправлены.');
+//     	}
+//  	});
+// }
